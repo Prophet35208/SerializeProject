8c7d7b2c6b5bcfa9b0ff8cb1c7e4cf0d3e295e58
Prophet35208 <taran.danil03@mail.ru>
2026-02-27 14:10:49 +0700

    Добавьте файлы проекта.

---
+++ SerializeProject/SerializeProject.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+
+struct ListNode { // ListNode модифицировать нельзя
+    ListNode* prev = nullptr; // указатель на предыдущий элемент или nullptr
+    ListNode* next = nullptr;
+    ListNode* rand = nullptr; // указатель на произвольный элемент данного списка, либо `nullptr`
+    std::string data; // произвольные пользовательские данные
+};
+
+int main()
+{
+    
+}
